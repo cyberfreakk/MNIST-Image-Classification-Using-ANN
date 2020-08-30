22e8efaf631dd8b0b5050265ffd2d07681bed7e9
Arunesh Gupta <49170977+cyberfreakk@users.noreply.github.com>
2020-08-30 19:17:29 +0530

    Update image_classifier.py

---
+++ image_classifier.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 15:58:08 2020
+
+@author: ARUNESH
+"""
+
+import sys
+import sklearn
+import tensorflow as tf
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+%matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+import tensorflow as tf
+from tensorflow import keras
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+
+X_train_full.shape
+X_train_full.dtype
+
+X_valid, X_train = X_train_full[:5000] / 255., X_train_full[5000:] / 255.
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test = X_test / 255.
+
+plt.imshow(X_train[0], cmap="binary")
+plt.axis('off')
+plt.show()
+
+y_train
+
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+class_names[y_train[0]]
+
+X_valid.shape
+X_test.shape
+
+n_rows = 4
+n_cols = 10
+plt.figure(figsize=(n_cols * 1.2, n_rows * 1.2))
+for row in range(n_rows):
+    for col in range(n_cols):
+        index = n_cols * row + col
+        plt.subplot(n_rows, n_cols, index + 1)
+        plt.imshow(X_train[index], cmap="binary", interpolation="nearest")
+        plt.axis('off')
+        plt.title(class_names[y_train[index]], fontsize=12)
+plt.subplots_adjust(wspace=0.2, hspace=0.5)
+save_fig('fashion_mnist_plot', tight_layout=False)
+plt.show()
+
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(300, activation="relu"))
+model.add(keras.layers.Dense(100, activation="relu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_random_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(300, activation="relu"),
+    keras.layers.Dense(100, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+model.layers
+
+model.summary()
+
+keras.utils.plot_model(model, "my_fashion_mnist_model.png", show_shapes=True)
+
+hidden1 = model.layers[1]
+hidden1.name
+
+model.get_layer(hidden1.name) is hidden1
+
+weights, biases = hidden1.get_weights()
+
+biases
+
+#using compile() to specify that the loss function and the optimizer to use
+model.compile(loss="sparse_categorical_crossentropy",optimizer="sgd",
+              metrics=["accuracy"])
+
+#training the model
+
+history = model.fit(X_train, y_train, epochs=30,
+                    validation_data=(X_valid, y_valid))
+
+import pandas as pd
+
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+save_fig("keras_learning_curves_plot")
+plt.show()
+
+model.evaluate(X_test, y_test)
+
+X_new = X_test[:3]
+y_proba = model.predict(X_new)
+y_proba.round(2)
+
+#predicting on first 3 instances of test set
+y_pred = model.predict_classes(X_new)
+y_pred
+
+np.array(class_names)[y_pred]
+
+y_new = y_test[:3]
+plt.figure(figsize=(7.2, 2.4))
+for index, image in enumerate(X_new):
+    plt.subplot(1, 3, index + 1)
+    plt.imshow(image, cmap="binary", interpolation="nearest")
+    plt.axis('off')
+    plt.title(class_names[y_test[index]], fontsize=12)
+plt.subplots_adjust(wspace=0.2, hspace=0.5)
+save_fig('fashion_mnist_images_plot', tight_layout=False)
+plt.show()
